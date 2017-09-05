a89627f460b32168b38d6f980eccb4b8cb8f55ea
Peter Novotnak <peter@novotnak.net>
2017-09-04 22:30:17 -0700

    moving over to a cache-backed watcher

--- main.go
+++ main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
 
+	"github.com/golang/glog"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/util/workqueue"
 
 	// Make GCP great again
+	"k8s.io/client-go/pkg/api/v1"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 )
 
@@ -17,6 +21,8 @@ var (
 )
 
 func main() {
+	defer glog.Flush()
+
 	// The parsed config file
 	var cfg Config
 	// Stop channels for all tainters
@@ -34,7 +40,50 @@ func main() {
 
 	// Attempt to connect
 	nodes, err := k8s.CoreV1().Nodes().List(metav1.ListOptions{})
-	log.Printf("Successfully connected to kubernetes, %v nodes online\n", len(nodes.Items))
+	glog.V(3).Infoln("Successfully connected to kubernetes, %v nodes online\n", len(nodes.Items))
+
+	// create the node watcher
+	nodeListWatcher := cache.NewListWatchFromClient(k8s.CoreV1().RESTClient(), "nodes", v1.NamespaceAll, fields.Everything())
+
+	// create the workqueue
+	queue := workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter())
+
+	// Bind the workqueue to a cache with the help of an informer. This way we make sure that
+	// whenever the cache is updated, the node key is added to the workqueue.
+	// Note that when we finally process the item from the workqueue, we might see a newer version
+	// of the Node than the version which was responsible for triggering the update.
+	indexer, informer := cache.NewIndexerInformer(nodeListWatcher, &v1.Node{}, 0, cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			key, err := cache.MetaNamespaceKeyFunc(obj)
+			if err == nil {
+				queue.Add(key)
+			}
+		},
+		UpdateFunc: func(old interface{}, new interface{}) {
+			key, err := cache.MetaNamespaceKeyFunc(new)
+			if err == nil {
+				queue.Add(key)
+			}
+		},
+		DeleteFunc: func(obj interface{}) {
+			// IndexerInformer uses a delta queue, therefore for deletes we have to use this
+			// key function.
+			key, err := cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
+			if err == nil {
+				queue.Add(key)
+			}
+		},
+	}, cache.Indexers{})
+
+	controller := NewController(queue, indexer, informer)
+
+	// Now let's start the controller
+	stop := make(chan struct{})
+	defer close(stop)
+	go controller.Run(2, stop)
+
+	// Wait forever
+	select {}
 
 	// Fire up a watcher
 	watch, err := k8s.CoreV1().Nodes().Watch(metav1.ListOptions{})
